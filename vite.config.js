9ecbc56083690252c5f8e54820e95eebb0a64210
Avishkar Deogharia <avishkardeogharia2004@gmail.com>
2024-10-26 15:26:21 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/', // Replace 'your-repo-name' with your actual GitHub repository name
+  // base: '/My-Portfolio/', // Replace 'your-repo-name' with your actual GitHub repository name
+  base: './',
 })
